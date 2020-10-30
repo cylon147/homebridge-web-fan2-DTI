fd9709586f2b791f0360aa770dff06f4b2648c5c
cylon147 <prakash@dtiautomation.com>
2020-10-30 19:07:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,94 @@
+<p align="center">
+  <a href="https://github.com/homebridge/homebridge"><img src="https://raw.githubusercontent.com/homebridge/branding/master/logos/homebridge-color-round-stylized.png" height="140"></a>
+</p>
+
+<span align="center">
+
+# homebridge-web-fan
+
+[![npm](https://img.shields.io/npm/v/homebridge-web-fan.svg)](https://www.npmjs.com/package/homebridge-web-fan) [![npm](https://img.shields.io/npm/dt/homebridge-web-fan.svg)](https://www.npmjs.com/package/homebridge-web-fan)
+
+</span>
+
+## Description
+
+This [homebridge](https://github.com/nfarina/homebridge) plugin exposes a web-based valve to Apple's [HomeKit](http://www.apple.com/ios/home/). Using simple HTTP requests, the plugin allows you to turn on/off the fan.
+
+## Installation
+
+1. Install [homebridge](https://github.com/nfarina/homebridge#installation-details)
+2. Install this plugin: `npm install -g homebridge-web-fan`
+3. Update your `config.json` file
+
+## Configuration
+
+```json
+"accessories": [
+     {
+       "accessory": "WebFan",
+       "name": "Fan",
+       "apiroute": "http://myurl.com"
+     }
+]
+```
+
+### Core
+| Key | Description | Default |
+| --- | --- | --- |
+| `accessory` | Must be `WebFan` | N/A |
+| `name` | Name to appear in the Home app | N/A |
+| `apiroute` | Root URL of your device | N/A |
+
+### Optional fields
+| Key | Description | Default |
+| --- | --- | --- |
+| `rotationSpeed` | Whether to expose rotation speed as a characteristic | `false` |
+| `rotationDirection` | Whether to expose rotation direction as a characteristic | `false` |
+
+### Additional options
+| Key | Description | Default |
+| --- | --- | --- |
+| `pollInterval` | Time (in seconds) between device polls | `300` |
+| `listener` | Whether to start a listener to get real-time changes from the device | `false` |
+| `timeout` | Time (in milliseconds) until the accessory will be marked as _Not Responding_ if it is unreachable | `3000` |
+| `port` | Port for your HTTP listener (if enabled) | `2000` |
+| `http_method` | HTTP method used to communicate with the device | `GET` |
+| `username` | Username if HTTP authentication is enabled | N/A |
+| `password` | Password if HTTP authentication is enabled | N/A |
+| `model` | Appears under the _Model_ field for the accessory | plugin |
+| `serial` | Appears under the _Serial_ field for the accessory | apiroute |
+| `manufacturer` | Appears under the _Manufacturer_ field for the accessory | author |
+| `firmware` | Appears under the _Firmware_ field for the accessory | version |
+
+## API Interfacing
+
+Your API should be able to:
+
+1. Return JSON information when it receives `/status`:
+```
+{
+    "currentState": INT_VALUE
+}
+```
+
+2. Set the state when it receives:
+```
+/setState?value=INT_VALUE
+```
+
+### Optional (if listener is enabled)
+
+1. Update `state` following a manual override by messaging the listen server:
+```
+/state?value=INT_VALUE
+```
+
+2. If `rotationSpeed` is enabled, update `rotationSpeed` following a manual override by messaging the listen server:
+```
+/rotationSpeed?value=INT_VALUE_0_TO_100
+```
+
+3. If `rotationDirection` is enabled, update `rotationDirection` following a manual override by messaging the listen server:
+```
+/rotationDirection?value=INT_VALUE_0_TO_1
+```
---
+++ index.js
@@ -0,0 +1,212 @@
+var Service, Characteristic
+const packageJson = require('./package.json')
+const request = require('request')
+const ip = require('ip')
+const http = require('http')
+
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-web-fan', 'WebFan', WebFan)
+}
+
+function WebFan (log, config) {
+  this.log = log
+
+  this.name = config.name
+  this.apiroute = config.apiroute
+  this.pollInterval = config.pollInterval || 15
+
+  this.listener = config.listener || false
+  this.port = config.port || 2000
+  this.requestArray = ['state', 'rotationSpeed', 'rotationDirection']
+
+  this.rotationSpeed = config.rotationSpeed || false
+  this.rotationDirection = config.rotationDirection || false
+
+  this.manufacturer = config.manufacturer || packageJson.author.name
+  this.serial = config.serial || this.apiroute
+  this.model = config.model || packageJson.name
+  this.firmware = config.firmware || packageJson.version
+
+  this.username = config.username || null
+  this.password = config.password || null
+  this.timeout = config.timeout || 3000
+  this.http_method = config.http_method || 'GET'
+
+  if (this.username != null && this.password != null) {
+    this.auth = {
+      user: this.username,
+      pass: this.password
+    }
+  }
+
+  if (this.listener) {
+    this.server = http.createServer(function (request, response) {
+      var baseURL = 'http://' + request.headers.host + '/'
+      var url = new URL(request.url, baseURL)
+      if (this.requestArray.includes(url.pathname.substr(1))) {
+        this.log.debug('Handling request')
+        response.end('Handling request')
+        this._httpHandler(url.pathname.substr(1), url.searchParams.get('value'))
+      } else {
+        this.log.warn('Invalid request: %s', request.url)
+        response.end('Invalid request')
+      }
+    }.bind(this))
+
+    this.server.listen(this.port, function () {
+      this.log('Listen server: http://%s:%s', ip.address(), this.port)
+    }.bind(this))
+  }
+
+  this.service = new Service.Fan(this.name)
+}
+
+WebFan.prototype = {
+
+  identify: function (callback) {
+    this.log('Identify requested!')
+    callback()
+  },
+
+  _httpRequest: function (url, body, method, callback) {
+    request({
+      url: url,
+      body: body,
+      method: this.http_method,
+      timeout: this.timeout,
+      rejectUnauthorized: false,
+      auth: this.auth
+    },
+    function (error, response, body) {
+      callback(error, response, body)
+    })
+  },
+
+  _getStatus: function (callback) {
+    var url = this.apiroute + '/status'
+    this.log.debug('Getting status: %s', url)
+
+    this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error getting status: %s', error.message)
+        this.service.getCharacteristic(Characteristic.On).updateValue(new Error('Polling failed'))
+        callback(error)
+      } else {
+        this.log.debug('Device response: %s', responseBody)
+        var json = JSON.parse(responseBody)
+        this.service.getCharacteristic(Characteristic.On).updateValue(json.currentState)
+        this.log.debug('Updated state to: %s', json.currentState)
+        if (this.rotationSpeed) {
+          this.service.getCharacteristic(Characteristic.RotationSpeed).updateValue(json.rotationSpeed)
+          this.log.debug('Updated rotationSpeed to: %s', json.rotationSpeed)
+        }
+        if (this.rotationDirection) {
+          this.service.getCharacteristic(Characteristic.RotationDirection).updateValue(json.rotationDirection)
+          this.log.debug('Updated rotationDirection to: %s', json.rotationDirection)
+        }
+        callback()
+      }
+    }.bind(this))
+  },
+
+  _httpHandler: function (characteristic, value) {
+    switch (characteristic) {
+      case 'state':
+        this.service.getCharacteristic(Characteristic.On).updateValue(value)
+        this.log('Updated %s to: %s', characteristic, value)
+        break
+      case 'rotationSpeed':
+        this.service.getCharacteristic(Characteristic.RotationSpeed).updateValue(value)
+        this.log('Updated %s to: %s', characteristic, value)
+        break
+      case 'rotationDirection':
+        this.service.getCharacteristic(Characteristic.RotationDirection).updateValue(value)
+        this.log('Updated %s to: %s', characteristic, value)
+        break
+      default:
+        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+    }
+  },
+
+  setOn: function (value, callback) {
+    value = value ? 1 : 0
+    var url = this.apiroute + '/setState?value=' + value
+    this.log.debug('Setting state: %s', url)
+
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error setting state: %s', error.message)
+        callback(error)
+      } else {
+        this.log('Set state to %s', value)
+        callback()
+      }
+    }.bind(this))
+  },
+
+  setRotationSpeed: function (value, callback) {
+    var url = this.apiroute + '/setRotationSpeed?value=' + value
+    this.log.debug('Setting rotationSpeed: %s', url)
+
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error setting rotationSpeed: %s', error.message)
+        callback(error)
+      } else {
+        this.log('Set rotationSpeed to %s', value)
+        callback()
+      }
+    }.bind(this))
+  },
+
+  setRotationDirection: function (value, callback) {
+    var url = this.apiroute + '/setRotationDirection?value=' + value
+    this.log.debug('Setting rotationDirection: %s', url)
+
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error setting rotationDirection: %s', error.message)
+        callback(error)
+      } else {
+        this.log('Set rotationDirection to %s', value)
+        callback()
+      }
+    }.bind(this))
+  },
+
+  getServices: function () {
+    this.informationService = new Service.AccessoryInformation()
+    this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, this.model)
+      .setCharacteristic(Characteristic.SerialNumber, this.serial)
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
+
+    this.service
+      .getCharacteristic(Characteristic.On)
+      .on('set', this.setOn.bind(this))
+
+    if (this.rotationSpeed) {
+      this.service
+        .getCharacteristic(Characteristic.RotationSpeed)
+        .on('set', this.setRotationSpeed.bind(this))
+    }
+
+    if (this.rotationDirection) {
+      this.service
+        .getCharacteristic(Characteristic.RotationDirection)
+        .on('set', this.setRotationDirection.bind(this))
+    }
+
+    this._getStatus(function () {})
+
+    setInterval(function () {
+      this._getStatus(function () {})
+    }.bind(this), this.pollInterval * 1000)
+
+    return [this.informationService, this.service]
+  }
+
+}
